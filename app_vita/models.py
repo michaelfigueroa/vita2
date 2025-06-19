1d4a4c1abffd5420fdc6602cb71613f3dd95f926
Michael Figueroa <michaelfigueroag@gmail.com>
2025-06-19 00:17:59 -0400

    Creación modelo property

--- app_vita/models.py
+++ app_vita/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+
+class Property(models.Model):
+    type = models.CharField(
+        verbose_name='Tipo',
+        max_length=12,
+        default=''
+    )
+    business= models.CharField(
+        verbose_name='Negocio',
+        max_length=12,
+        default=''
+    )
+    model = models.CharField(
+        verbose_name='Modelo',
+        max_length=12,
+        default=''
+    )
+    orientation = models.CharField(
+        verbose_name='Orientación',
+        max_length=12,
+        default=''
+    )
+    dimension = models.FloatField(
+        verbose_name='Metraje útil',
+        max_length=12,
+        default=0
+    )
+    dimension_total = models.FloatField(
+        verbose_name='Metraje total',
+        max_length=5,
+        default=0
+    )
+    status = models.CharField(
+        verbose_name='Estado',
+        max_length=5,
+        default=''
+    )
+    washing = models.CharField(
+        verbose_name='Lavadora',
+        max_length=12,
+        default='Si'
+    )
+    floor = models.IntegerField(
+        verbose_name='Piso',
+        default=1
+    )
